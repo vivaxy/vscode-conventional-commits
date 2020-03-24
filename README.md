3dec3d5c86a304e5f354d7d464d3633bbc92250f
vivaxy <xyxuye2007@126.com>
2020-03-24 23:03:41 +0800

    feat: :sparkles:  initial commit

--- README.md
+++ README.md
@@ -1,76 +1,23 @@
-# vivaxy-vscode-conventional-commits README
+# VSCode Conventional Commits
 
-This is the README for your extension "vivaxy-vscode-conventional-commits".
-After writing up a brief description, we recommend including the following
-sections.
+[Conventional Commits](https://www.conventionalcommits.org/) for VSCode.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your
-extension in action. Image paths are relative to this README file.
+This extension helps you to fill in commit message according to
+[Conventional Commits](https://www.conventionalcommits.org/).
 
-For example if there is an image subfolder under your extension project
-workspace:
+![Demo](./assets/docs/demo.gif)
 
-\!\[feature X\]\(images/feature-x.png\)
+You can access VSCode Conventional Commits in two ways:
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to
-> show off your extension! We recommend short, focused animations that are easy
-> to follow.
+1. `Command + Shift + P` or `Ctrl + Shift + P`, enter `Conventional Commits`,
+   and press `Enter`.
+2. Click the icon on the Source Control menu. See the image below.
 
-## Requirements
+![Icon on the Source Control menu](./assets/docs/icon-on-the-source-control-menu.png)
 
-If you have any requirements or dependencies, add a section describing those and
-how to install and configure them.
+# Prior Art
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the
-`contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-- `myExtension.enable`: enable/disable this extension
-- `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against
-your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code. Here are some
-useful editor keyboard shortcuts:
-
-- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-- Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-- Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of
-  Markdown snippets
-
-### For more information
-
-- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- [Commit Tagger](https://github.com/Mongkii/Commit-Tagger)
+- [gacp](https://github.com/vivaxy/gacp)
--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,175 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as VSCodeGit from './vendors/git';
+import gitmojis from './vendors/gitmojis';
+const conventionalCommitsTypes = require('conventional-commit-types');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vivaxy-vscode-conventional-commits" is now active!',
+function getGitAPI(): VSCodeGit.API | void {
+  const vscodeGit = vscode.extensions.getExtension<VSCodeGit.GitExtension>(
+    'vscode.git',
   );
+  if (vscodeGit) {
+    return vscodeGit.exports.getAPI(1);
+  }
+}
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'extension.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
+enum QUESTION_TYPES {
+  QUICK_PICK,
+  INPUT_BOX,
+}
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World!');
+type Question = {
+  type: QUESTION_TYPES;
+  name: string;
+  placeholder: string;
+  items?: { label: string; detail: string; description: string }[];
+  format?: (input: string) => string;
+};
+
+async function prompt() {
+  const questions: Question[] = [
+    {
+      type: QUESTION_TYPES.QUICK_PICK,
+      name: 'type',
+      placeholder: "Select the type of change that you're committing",
+      items: Object.keys(conventionalCommitsTypes.types).map(function (type) {
+        const { title, description } = conventionalCommitsTypes.types[type];
+        return {
+          label: type,
+          description: title,
+          detail: description,
+        };
+      }),
+    },
+    {
+      type: QUESTION_TYPES.INPUT_BOX,
+      name: 'scope',
+      placeholder: 'Denote the scope of this change',
+    },
+    {
+      type: QUESTION_TYPES.QUICK_PICK,
+      name: 'gitmoji',
+      placeholder: 'Choose a gitmoji',
+      items: [
+        {
+          label: 'none',
+          description: '',
+          detail: 'No gitmoji',
+        },
+        ...gitmojis.gitmojis.map(function ({ emoji, code, description }) {
+          return {
+            label: code,
+            description: emoji,
+            detail: description,
+          };
+        }),
+      ],
+      format(input) {
+        if (input === 'none') {
+          return '';
+        }
+        return input;
+      },
+    },
+    {
+      type: QUESTION_TYPES.INPUT_BOX,
+      name: 'subject',
+      placeholder: 'Write a short, imperative tense description of the change',
+    },
+    {
+      type: QUESTION_TYPES.INPUT_BOX,
+      name: 'body',
+      placeholder: 'Provide a longer description of the change',
+    },
+    {
+      type: QUESTION_TYPES.INPUT_BOX,
+      name: 'footer',
+      placeholder: 'List any breaking changes or issues closed by this change',
+    },
+  ];
+
+  function createQuickPick({
+    placeholder,
+    items = [],
+    format = (i) => i,
+  }: Question) {
+    return new Promise(function (resolve, reject) {
+      const picker = vscode.window.createQuickPick();
+      picker.placeholder = placeholder;
+      picker.matchOnDescription = true;
+      picker.matchOnDetail = true;
+      picker.items = items;
+      picker.show();
+      picker.onDidAccept(function () {
+        const result = format(picker.selectedItems[0].label);
+        picker.dispose();
+        resolve(result);
+      });
+    });
+  }
+
+  function createInputBox({ placeholder, format = (i) => i }: Question) {
+    return new Promise(function (resolve, reject) {
+      const input = vscode.window.createInputBox();
+      input.placeholder = placeholder;
+      input.onDidAccept(function () {
+        const result = format(input.value);
+        input.dispose();
+        resolve(result);
+      });
+      input.prompt = placeholder;
+      input.show();
+    });
+  }
+
+  let answers: Record<string, string> = {};
+
+  for (const question of questions) {
+    if (question.type === QUESTION_TYPES.QUICK_PICK) {
+      // @ts-ignore
+      answers[question.name] = await createQuickPick(question);
+    }
+    if (question.type === QUESTION_TYPES.INPUT_BOX) {
+      // @ts-ignore
+      answers[question.name] = await createInputBox(question);
+    }
+  }
+
+  console.log('answers', answers);
+  return answers;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    'extension.conventionalCommits',
+    async function (uri) {
+      const git = getGitAPI();
+      if (!git) {
+        vscode.window.showErrorMessage('vscode.git is not enabled');
+        return;
+      }
+      const answers = await prompt();
+      vscode.commands.executeCommand('workbench.view.scm');
+      git.repositories.forEach(function (repo) {
+        let message = '';
+        message += answers.type.trim();
+        if (answers.scope) {
+          message += `(${answers.scope})`;
+        }
+        message += ': ';
+        if (answers.gitmoji) {
+          message += `${answers.gitmoji} `;
+        }
+        message += answers.subject.trim();
+        message += '\n\n';
+        message += answers.body.trim();
+        message += '\n\n';
+        message += answers.footer.trim();
+        repo.inputBox.value = message;
+      });
     },
   );
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
