60e7415057add068f94ae89e13f3575627ce832b
yi_Xu <xjy123456120@163.com>
2021-03-12 21:00:02 +0800

    feat: :package: use webpack to bundle extension (#78)

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                sourceMap: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  stats: {
+    warnings: false,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from:
+            'node_modules/@yi-xu-0100/conventional-commit-types-i18n/lib/locale/',
+          to: '../locale/',
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
