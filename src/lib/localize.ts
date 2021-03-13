f331c24cbc1eb69d51115c968cb3c9eef1dcce99
yi_Xu <xjy123456120@163.com>
2021-03-13 10:55:27 +0800

    fix(localization): :globe_with_meridians: fix localization missing by using the webpack
    Use `extensions.getExtension` to get the extension root directory.
    
    Added error output about missing translations.
    
    Use `readFileSync` instead of `require` to prevent webpack from bundling `json` files repeatedly.
    
    Add `clean-webpack-plugin` to clean up the cache generated during packaging.

--- src/lib/localize.ts
+++ src/lib/localize.ts
@@ -2,41 +2,72 @@
  * @since 2020-10-09 15:46
  * @author vivaxy
  */
-import * as vscode from 'vscode';
-import * as output from './output';
-import * as path from 'path';
+import { extensions, env, window } from 'vscode';
+import { appendLine } from './output';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
 
 const DEFAULT_LOCALE = 'en';
 
+export const rootPath = getRoot('vivaxy.vscode-conventional-commits');
+
+function getRoot(name: string) {
+  const extension = extensions.getExtension(name);
+  if (extension) return extension.extensionPath;
+  else {
+    window.showErrorMessage(
+      `undefined: ENOENT: no such extension, getExtension(${name})`,
+    );
+    return resolve(__dirname, '../');
+  }
+}
+
 export let locale: string = DEFAULT_LOCALE;
-let defaultNLS: Record<string, string> = {};
 let localeNLS: Record<string, string> = {};
+let defaultNLS: Record<string, string> = {};
 
 function getLocale() {
-  // const config = JSON.parse(process.env.VSCODE_NLS_CONFIG!);
-  // return config.locale;
-  const language = vscode.env.language;
-  output.appendLine('vscode.env.language: ' + language);
+  const language = env.language;
+  appendLine('vscode.env.language: ' + language);
   return language || DEFAULT_LOCALE;
 }
 
 export function initialize() {
-  defaultNLS = getNLS();
   locale = getLocale();
+  defaultNLS = getNLS();
   localeNLS = getNLS(locale);
 }
 
 export default function localize(key: string) {
-  return localeNLS[key] || defaultNLS[key]; // || `Missing translation for ${key}`;
+  if (localeNLS[key]) return localeNLS[key];
+  else {
+    appendLine(
+      `[warning] localize: Missing translation for ${key}, try to use default localization.`,
+    );
+    if (defaultNLS[key]) return defaultNLS[key];
+    else {
+      window.showErrorMessage(
+        `[error] localize: Missing default translation for ${key}.`,
+      );
+      return `Missing default translation for ${key}.`;
+    }
+  }
 }
 
 function getNLS(locale: string = '') {
-  switch (locale) {
-    case 'zh-cn': {
-      return require('../../package.nls.zh-cn.json');
-    }
-    default: {
-      return require('../../package.nls.json');
-    }
+  const suffix = (locale && `.${locale}`) ?? '';
+  let content = '';
+  try {
+    content = readFileSync(
+      resolve(rootPath, `package.nls${suffix}.json`),
+      'utf-8',
+    );
+  } catch (e) {
+    if (e.message.includes('ENOENT: no such file or directory')) {
+      appendLine(`[warning] getNLS: Missing translation for ${locale}`);
+    } else window.showErrorMessage(`[error] getNLS: ${e}`);
+    content = readFileSync(resolve(rootPath, `package.nls.json`), 'utf-8');
+  } finally {
+    return JSON.parse(content);
   }
 }
