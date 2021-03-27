fd165d9a264ad07e722c3ff84b18c7638386e35e
yi_Xu <xjy123456120@163.com>
2021-03-27 19:34:19 +0800

    refactor(output): :recycle: add log function for the output module

--- src/lib/output.ts
+++ src/lib/output.ts
@@ -3,6 +3,7 @@
  * @author vivaxy
  */
 import * as vscode from 'vscode';
+import localize from './localize';
 
 let output: vscode.OutputChannel;
 
@@ -12,4 +13,27 @@ export function initialize() {
 
 export function appendLine(message: string) {
   output.appendLine(message);
+}
+
+export function info(message: string) {
+  appendLine('[info] ' + message);
+}
+
+export function warning(message: string) {
+  appendLine('[warning] ' + message);
+}
+
+// give error or message of the custom error
+export function error(
+  functionName: string,
+  arg: string | Error,
+  isBreaking?: boolean,
+) {
+  const message = typeof arg === 'string' ? arg : arg.stack;
+  appendLine(`[error] ${functionName}: ${message}`);
+
+  const title = localize('extension.name');
+  const body = typeof arg === 'string' ? arg : arg.message;
+  vscode.window.showErrorMessage(`${title}: ${body}`);
+  if (isBreaking) throw new Error('custom breaking error has been catch!');
 }
