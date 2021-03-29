3bcdf8dcc01bf0eb3933f5cd9fb1cad178da7c59
yi_Xu <xjy123456120@163.com>
2021-03-29 13:42:06 +0800

    refactor(localize): :recycle: refactor localize.ts

--- src/lib/localize.ts
+++ src/lib/localize.ts
@@ -2,12 +2,10 @@
  * @since 2020-10-09 15:46
  * @author vivaxy
  */
-import { extensions, env, window } from 'vscode';
-import { appendLine } from './output';
+import { extensions, env } from 'vscode';
+import * as output from './output';
 import { resolve } from 'path';
 import { readFileSync } from 'fs';
-
-const DEFAULT_LOCALE = 'en';
 
 export const rootPath = getRoot('vivaxy.vscode-conventional-commits');
 
@@ -15,20 +13,20 @@ function getRoot(name: string) {
   const extension = extensions.getExtension(name);
   if (extension) return extension.extensionPath;
   else {
-    window.showErrorMessage(
-      `undefined: ENOENT: no such extension, getExtension(${name})`,
-    );
+    // It does not have an output channel when activating the extension.
     return resolve(__dirname, '../');
   }
 }
 
+const DEFAULT_LOCALE = 'en';
 export let locale: string = DEFAULT_LOCALE;
+
 let localeNLS: Record<string, string> = {};
 let defaultNLS: Record<string, string> = {};
 
 function getLocale() {
   const language = env.language;
-  appendLine('vscode.env.language: ' + language);
+  output.info('vscode.env.language: ' + language);
   return language || DEFAULT_LOCALE;
 }
 
@@ -41,31 +39,30 @@ export function initialize() {
 export default function localize(key: string) {
   if (localeNLS[key]) return localeNLS[key];
   else {
-    appendLine(
-      `[warning] localize: Missing translation for ${key}, try to use default localization.`,
+    output.warning(
+      `localize: Missing translation for ${key}, try to use default localization.`,
     );
     if (defaultNLS[key]) return defaultNLS[key];
     else {
-      window.showErrorMessage(
-        `[error] localize: Missing default translation for ${key}.`,
-      );
+      // Not break and return prompt message.
+      output.error('localize', `Missing default translation for ${key}.`);
       return `Missing default translation for ${key}.`;
     }
   }
 }
 
 function getNLS(locale: string = '') {
-  const suffix = (locale && `.${locale}`) ?? '';
+  const packageName = `package.nls${(locale && `.${locale}`) ?? ''}.json`;
   let content = '';
   try {
-    content = readFileSync(
-      resolve(rootPath, `package.nls${suffix}.json`),
-      'utf-8',
-    );
+    content = readFileSync(resolve(rootPath, packageName), 'utf-8');
   } catch (e) {
-    if (e.message.includes('ENOENT: no such file or directory')) {
-      appendLine(`[warning] getNLS: Missing translation for ${locale}`);
-    } else window.showErrorMessage(`[error] getNLS: ${e}`);
+    if (!e.message.includes('ENOENT: no such file or directory')) {
+      // Not break and try to use default package.nls.json
+      output.error('getNLS', e);
+    } else if (locale != 'en') {
+      output.warning(`getNLS: Missing translation for ${locale}`);
+    }
     content = readFileSync(resolve(rootPath, `package.nls.json`), 'utf-8');
   } finally {
     return JSON.parse(content);
