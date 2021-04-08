d9e11186b35e96fd92fda02d854de660df59efc9
yi_Xu <xjy123456120@163.com>
2021-04-08 15:23:29 +0800

    feat(extension): :sparkles: add two commands about note
    1. Add `extension.conventionalCommits.resetGlobalState` to reset global state of version.
    2. Add `extension.conventionalCommits.showNewVersionNotes` to show new version notes.

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,34 @@
 import * as vscode from 'vscode';
 import createConventionalCommits from './lib/conventional-commits';
 import * as output from './lib/output';
-import * as localize from './lib/localize';
+import localize, {
+  getSourcesLocalize,
+  initialize as localizeInitialize,
+} from './lib/localize';
 import CommitProvider from './lib/editor/provider';
 import { ID } from './configs/keys';
 
 export async function activate(context: vscode.ExtensionContext) {
   output.initialize();
   output.info('Extension Activated');
-  localize.initialize();
+  localizeInitialize();
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'extension.conventionalCommits',
       createConventionalCommits(),
+    ),
+    vscode.commands.registerCommand(
+      'extension.conventionalCommits.resetGlobalState',
+      () => {
+        context.globalState.update('version', '0.0.0');
+        const title = localize('extension.name');
+        const message = getSourcesLocalize('resetMessage');
+        vscode.window.showInformationMessage(`${title}: ${message}`);
+      },
+    ),
+    vscode.commands.registerCommand(
+      'extension.conventionalCommits.showNewVersionNotes',
+      () => output.showNewVersionNotes(ID, context, true),
     ),
   );
   output.showNewVersionNotes(ID, context);
